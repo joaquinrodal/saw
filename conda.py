d31573e2436e93d2dfe9828edf1ec57bbbe9c7f2
JOAQUIN RODAL <informatico.joaquin@gmail.com>
2023-09-09 21:10:20 +0100

    Create conda.py

---
+++ conda.py
@@ -0,0 +1,20 @@
+#----------------------
+# conda
+
+conda create -n saturno python=3.10 
+conda env list
+conda activate saturno
+
+sudo apt install zip
+# Comprimir una carpeta con todo su contenido
+zip –r nombre_archivo.zip directorio_a_comprimir
+
+
+sudo apt install unzip
+# descomprimir archivo
+unzip nombre_archivo.zip 
+
+-- hay que ir a miniconda3 / envs 
+y comprimir esta carpeta.
+
+y llevarlo a MEGA
