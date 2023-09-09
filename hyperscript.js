7244a4585e0b216ea6a6833b778167c7e0d0f006
JOAQUIN RODAL <informatico.joaquin@gmail.com>
2023-09-09 23:38:23 +0100

    Update hyperscript.js

--- hyperscript.js
+++ hyperscript.js
@@ -5,3 +5,16 @@
   </div>
 
  cuandas hagas click cambia la clase color de fondo azul sobre el mismo elemento.
+
+  AL PULSAR CLICK EN UN BOTON REMOVER UNA CLASE DE UN DIV
+
+_="on click remove .bg-blue-400 from #etiqueta
+
+   esto esta genial --> al pulsar quito el color de fondo de otro div y hago desaparecer el div que lo genera.
+    
+   <div _="on click remove .bg-yellow-400 from #etiqueta then add .hidden to me">
+        QUITAR FONDO DE UN DIV
+    </div>
+    <div id="etiqueta" class="bg-yellow-400">
+        FONDO DE COLOR AMARILLO
+    </div>
