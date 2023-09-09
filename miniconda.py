9c735c6c05f5629c569eeb6eb43218e11561913f
JOAQUIN RODAL <informatico.joaquin@gmail.com>
2023-09-09 21:07:57 +0100

    Create miniconda.py

---
+++ miniconda.py
@@ -0,0 +1,20 @@
+#--------------------------------
+# MINICONDA
+
+se descarga el archivo 
+y se sube al servidor
+
+con la sesion actual 
+
+bash miniconda.sh  
+
+e instalamos miniconda 
+que es para crear entornos virtuales de python
+
+# ----------------
+# crear un entorno virtual 
+
+conda create -n saturno python=3.11
+
+conda activate saturno 
+conda deactivate 
