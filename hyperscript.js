198646a1870dc2691bd7fb28631d0ba0b6222f80
JOAQUIN RODAL <informatico.joaquin@gmail.com>
2023-09-09 23:26:40 +0100

    Create hyperscript.js

---
+++ hyperscript.js
@@ -0,0 +1,7 @@
+<script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
+
+ <div _="on click toggle .bg-blue-400 on me">
+        HOLA MUNDO
+  </div>
+
+ cuandas hagas click cambia la clase color de fondo azul sobre el mismo elemento.
