51e423b8e0841623b1509da382ecade8c20713ea
JOAQUIN RODAL <informatico.joaquin@gmail.com>
2023-10-08 23:24:47 +0100

    Update localstore.js

--- localstore.js
+++ localstore.js
@@ -14,3 +14,24 @@ almacenamos el valor en el localstorage
 
 cuando cargamos la pagina 
 cargara el valor almacenado en el localstorage..
+
+Nueva Forma de hacerlo 
+
+  contador:localStorage.getItem("contador") || 0,
+
+  start: function(){
+      
+       this.$watch('contador', (value, oldValue) => localStorage.setItem("contador", value))
+
+
+    },
+
+   <button x-on:click="contador++;
+                               ">contador</button>
+
+   de esta forma es mucho mejor , porque de cualquier parte de la aplicacion
+   de cualquier ventana que modifique el valor contador se almacenara automaticamente
+   en el localstorage 
+   y cuando carga la pagina , se actualiza el valor contador.
+
+   genial... reto conseguido y muy facil de entender..
