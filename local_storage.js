a03aab272a5da379aac407b5b0abf22cdb010029
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-11-17 21:15:49 +0000

    Create local_storage.js

---
+++ local_storage.js
@@ -0,0 +1,7 @@
+Para obtener datos del Localstore
+contador:localStorage.getItem("contador") || 0
+
+si hay informacion en el localstore lo coge sino le asignamos un 0.
+
+var valorAAlmacenar = 'miValor';
+localStorage.setItem('clave', valorAAlmacenar)
