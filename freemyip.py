bab271c0d9852f5e6c5c74611f070d0fec6e16c6
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-11-26 18:31:10 +0000

    Create freemyip.py

---
+++ freemyip.py
@@ -0,0 +1,10 @@
+
+#-----------------
+crmmovil.freemyip.com 
+https://freemyip.com/update?token=b2c33cc5a15dfd3f13589e0f&domain=crmmovil.freemyip.com 
+url
+https://freemyip.com/help?domain=crmmovil.freemyip.com&token=b2c33cc5a15dfd3f13589e0f
+
+curl https://freemyip.com/update?token=b2c33cc5a15dfd3f13589e0f&domain=crmmovil.freemyip.com 
+curl https://freemyip.com/update?token=b2c33cc5a15dfd3f13589e0f&domain=crmmovil.freemyip.com&txt=null
+
