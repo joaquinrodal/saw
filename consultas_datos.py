0be9eaf8776e637eb95cf1aa5cb1294bb5021b23
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-12-08 13:38:12 +0000

    Create consultas_datos.py

---
+++ consultas_datos.py
@@ -0,0 +1,26 @@
+
+#------------------------------------------
+
+usuarios = filter(lambda usuario:usuario.id == id , usuario_lista) 
+
+lambda funcion anonima
+
+
+try:
+     list(usuarios)[0] 
+except:
+   return {
+     'error':' usuario no encontrado '
+   }
+
+filter(funcion,lista)
+filter solo coje cada elemento de la lista , donde cada elemento se la aplica una funcion donde la condicion sea verdadera.
+
+lambda  lambda argumentos: expresion 
+funciones anonimas 
+
+lambda argumentos : expresion   = funcion .  
+filter(funcion,lista) 
+
+
+
