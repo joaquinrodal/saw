23e6d5ec03d6f4084e52e4328574927b62d2caff
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-12-13 18:59:38 +0000

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -4,7 +4,7 @@ FASTAPI --> TELEGRAM VIA WEBHOOK
 @app.post('/webhook')
 # este es el token de crmmovil_bot
 # t.me/crmmovil_bot
-# 6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw
+# 
 # https://api.telegram.org/bot<token>/setWebhook?url=<tu_url_webhook> 
 # https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/setWebhook?url=https://crmmovil.freemyip.com/webhook
 # https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/getWebhookInfo
@@ -19,7 +19,7 @@ async def webhook(request : Request):
         texto = mensaje.split('-')
         usuario = db.query(Usuario).filter(Usuario.chat_id == chat_id).first()
         if usuario :
-            url = "https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/sendMessage"
+            url = "https://api.telegram.org//sendMessage"
             async with httpx.AsyncClient() as client:
                 nombre = message["message"]["from"]["first_name"]
                 salida = f'Hola {nombre}, Ya estas registrado en la aplicacion...'
@@ -30,20 +30,20 @@ async def webhook(request : Request):
                 if usuario:
                     usuario.chat_id = chat_id
                     db.commit()
-                    url = "https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/sendMessage"
+                    url = "https://api.telegram.org/sendMessage"
                     async with httpx.AsyncClient() as client:
                         nombre = message["message"]["from"]["first_name"]
                         salida = f'Hola {nombre}, Ya estas registrado en la aplicacion...'
                         response = await client.post(url=url , json={"chat_id":chat_id,"text":salida})
                 else:
-                    url = "https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/sendMessage"
+                    url = "https://api.telegram.org//sendMessage"
                     async with httpx.AsyncClient() as client:
                         nombre = message["message"]["from"]["first_name"]
                         salida = f'Hola {nombre}, No estas dado de alta en la aplicacion CRMMOVIL, REGISTRATE...'
                         response = await client.post(url=url , json={"chat_id":chat_id,"text":salida})
             else:
  
-                url = "https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/sendMessage"
+                url = "https://api.telegram.org/sendMessage"
                 async with httpx.AsyncClient() as client:
                     nombre = message["message"]["from"]["first_name"]
                     salida = f'Hola {nombre}, no estas registrado en la aplicacion, tienes que registrarte primero. para ello tienes que mandarme el siguiente codigo : X-dni , sustituye dni por tu dni con la letra al final'
@@ -53,13 +53,13 @@ async def webhook(request : Request):
         print('chat_id:->',chat_id)
         print('MENSAJE:->',mensaje)
 
-        #url = "https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/sendMessage"
+        #url = "https://api.telegram.org//sendMessage"
         #async with httpx.AsyncClient() as client:
             #nombre = message["message"]["from"]["first_name"]
             #salida = f'Hola {nombre}, soy el servidor que deseas ..'
             #response = await client.post(url=url , json={"chat_id":chat_id,"text":salida})
 
-        #url = "https://api.telegram.org/bot6554423575:AAEvXfuNE5qpf162twBgPfTWc1UQpyjdnCw/sendPhoto"
+        #url = "https://api.telegram.org/sendPhoto"
         #async with httpx.AsyncClient() as client2:
             #url_foto = 'https://crmmovil.freemyip.com/static/documentos/18-portada.png'
             #response2 = await client2.post(url=url , json={"chat_id":chat_id,"photo":url_foto})
