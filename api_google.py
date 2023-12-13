384638e760b9014a1ad7ec9c84c5371a29631838
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-12-13 19:08:49 +0000

    Update api_google.py

--- api_google.py
+++ api_google.py
@@ -1,6 +1,9 @@
 video
 https://www.youtube.com/watch?v=OwE6EjKn4oM
 
+codigo
+https://github.com/novax124/Asistente-Personal-MIKE-/blob/main/GOOGLE/GMAIL/GMAILMIKE.py
+
 
 crear cuanta de correo en google .
 ir a google cloud console
