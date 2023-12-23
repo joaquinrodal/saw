72b85926f1009df93bb2ec411d470fe6bbeec5de
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-12-23 21:13:05 +0000

    Create contenedor_docker_nuevo.py

---
+++ contenedor_docker_nuevo.py
@@ -0,0 +1,7 @@
+
+# Cuando creamos un contenedor docker 
+y entramos en modo root
+
+service mariadb start    --> arrancamos el servidor mariadb
+netstat -tanp  --> comprobamos que está levantado el puerto 3306
+
