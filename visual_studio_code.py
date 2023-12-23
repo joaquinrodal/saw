70a247480719a615e9a718670d63d7c8835ad49c
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-12-23 09:39:29 +0000

    Create visual_studio_code.py

---
+++ visual_studio_code.py
@@ -0,0 +1,27 @@
+
+
+se instala en root
+
+
+es6-string-html 
+
+
+en configuracion 
+extensiones
+emmet 
+setting.son
+
+{
+    "files.exclude": {
+        ".*": true
+    },
+    "workbench.colorTheme": "Visual Studio Dark",
+    "editor.fontSize": 18,
+    "terminal.integrated.fontSize": 18,
+    "emmet.includeLanguages": {
+        "javascript": "html"
+    },
+    "files.autoSave": "off",
+    "workbench.iconTheme": "vscode-icons",
+    "keyboard.layout": "com.apple.keylayout.Spanish-ISO"
+}
