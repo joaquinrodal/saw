8f6c82b66da1432cea8aff74da9782754b493e61
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2023-12-27 12:18:47 +0000

    Update exclude_unset.py

--- exclude_unset.py
+++ exclude_unset.py
@@ -5,3 +5,7 @@ En SQLModel, exclude_unset=True es un parámetro que puedes utilizar al llamar a
 en un modelo para excluir los campos que no han sido establecidos en la instancia del modelo. 
 Esto es útil cuando deseas convertir un modelo a un diccionario y deseas omitir los campos 
 que tienen el valor predeterminado y no han sido modificados.
+
+    data_centro_trabajo = centro_trabajo.dict(exclude_unset=True)
+    for key, value in data_centro_trabajo.items():
+        setattr(db_centro_trabajo, key, value)
