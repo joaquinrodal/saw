3cad19d823e6f198daa69476c1c7ad252cfef323
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-02-09 16:02:08 +0000

    Create cosas_fastapi.py

---
+++ cosas_fastapi.py
@@ -0,0 +1,23 @@
+
+
+pip install fastapi-paginate[all] 
+SQLAlchemy>=1.3.20
+
+pip install hypercorn 
+hypercorn -b 0.0.0.0:8200 servidor:app 
+
+
+
+pip install daphne
+daphne -b 0.0.0.0 -p 8200 servidor:app
+
+
+#----------------------------------------
+gunicorn -b 0.0.0.0:8000 your_module:app
+-k uvicorn.workers.UvicornWorker
+#-----------------------------------------
+
+
+
+pip install gunicorn 
+gunicorn -b 0.0.0.0:8200 -k uvicorn.workers.UvicornWorker servidor:app
