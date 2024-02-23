cf5d103b9b73c7657bcef8afdf3852b05b076747
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-02-23 21:53:09 +0000

    Update htmx.js

--- htmx.js
+++ htmx.js
@@ -7,3 +7,22 @@ CARGAR CADA 5 SEGUNDO ESTE DIV
 </div>
 
 <div hx-get="/news" hx-trigger="every 2s"></div>
+
+
+
+CON ALPINE JS 
+
+EN UNA PLANTILLA DE TEMPLATE , no funciona htmx porque ???
+x-init="console.log('cargado htmx',htmx.version);htmx.process($refs.contenido);htmx.process($refs.contenido2); cargar();"
+
+       <div x-ref="contenido2"
+            hx-post="/datos_servidor2" hx-target="#resultado" hx-trigger="click">enviar</div>
+
+     <div  x-ref="contenido"
+            id="nuevo-contenido"
+            hx-target="this" hx-post="/datos_servidor2" hx-trigger="every 10s"></div>
+
+
+como crear un evento con alpine 
+  <div @men.window="console.log('EVENTO CURIOSO')"></div>
+      <div @click="$dispatch('men')">Enviar</div>
