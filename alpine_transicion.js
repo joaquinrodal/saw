981ea139d60f04c3f526a787b1f5782651c20edd
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-02-23 21:36:24 +0000

    Create alpine_transicion.js

---
+++ alpine_transicion.js
@@ -0,0 +1,14 @@
+
+<div x-data="{ open: false }">
+    <button @click="open = ! open">Toggle</button>
+ 
+    <div
+        x-show="open"
+        x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 scale-90"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-90"
+    >Hello 👋</div>
+</div>
