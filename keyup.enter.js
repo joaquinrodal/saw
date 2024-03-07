d7533b194fb7c1f3a37b9c7d0ac9615b311dc301
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-03-07 00:07:53 +0000

    Create keyup.enter.js

---
+++ keyup.enter.js
@@ -0,0 +1,14 @@
+
+
+        start: function(){
+           
+            window.addEventListener('keyup',(e)=>{
+                if(e.key == 'Enter'){
+                    this.enviar();
+                    console.log('has presionado Enter')
+                }
+             
+               
+             })
+            
+        },
