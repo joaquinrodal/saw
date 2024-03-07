19dc5476d50b320c87063d9356e7017299605f40
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-03-07 00:32:21 +0000

    Update eventos_js.js

--- eventos_js.js
+++ eventos_js.js
@@ -1,2 +1,14 @@
 
 
+            window.addEventListener('offline',(e)=>{
+
+              console.log('NO HAY CONEXION A INTERNET')
+               
+             })
+
+
+            window.addEventListener('online',(e)=>{
+
+              console.log('si HAY CONEXION A INTERNET')
+               
+             })
