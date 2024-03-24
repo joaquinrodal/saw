49e7629427cd43548ffa84c5ba44be7e173d02b1
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-03-24 16:49:22 +0000

    Create resaltar_html_string.js

---
+++ resaltar_html_string.js
@@ -0,0 +1,38 @@
+# INSTALAR 
+es6-string-html
+EN VISUAL STUDIO CODE
+
+Y LUEGO ANTES DE ESCRIBIT UN STRING CON ``
+/*HTML*/
+                 
+
+    informacion :/*html*/ `
+                    <div  x-init="numero +=1"
+                          x-ref="importacion"
+                
+                class="absolute top-[20%] left-[35%] bg-white 
+                        w-[600px] h-[400px] z-[200] 
+                        flex justify-center items-center border rounded-[5px]">
+
+                <div class="flex justify-center items-center flex-col ">
+                    <img   class="w-[50px] h-[50px] absolute top-[15px] left-[20px]"
+                            src="/static/imagenes/logo_ew_verde2.png" alt="">
+                    <div style="font-family: 'Rajdhani';font-weight: 300;" 
+                        class="text-[35px] absolute top-[15px]">
+                        Información
+                    </div>
+                    <div  style="font-family: 'Rajdhani';font-weight: 300;"
+                           class="text-[24px]"
+                           x-text="mensaje_informacion">
+                   
+                    </div>
+                    <div    @click="codigo = ''"
+                            class="cursor-pointer hover:scale-[1.09] absolute bottom-[20px]
+                            text-[20px] text-white bg-[#F6921F] px-[30px] border rounded-[5px] py-[10px]">
+                    Aceptar
+                    </div>
+                </div>
+
+                </div>
+    
+    `
