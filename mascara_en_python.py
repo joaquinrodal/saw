960c0b255eda26b50f78b40dc78ad44777f1db75
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-04-16 00:51:55 +0100

    Create mascara_en_python.py

---
+++ mascara_en_python.py
@@ -0,0 +1,11 @@
+
+
+f'{numero::_>10}' texto a la derecha y los que falta con _
+f'{numero::_<10}' texto a la izquierda
+f'{numero::_^10}' texto en el medio
+f'{numero::0>10}'
+f'{year}-{month:0>2}-{day:0>2}'
+
+En ambos casos, :.2f indica que deseas mostrar el número con dos decimales. 
+Puedes ajustar el número 2 al número deseado de decimales que quieras mostrar. 
+Por ejemplo, si deseas mostrar tres decimales, simplemente cambia 2 a 3.
