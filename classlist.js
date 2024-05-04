6a5af9bab036fde01b01b0573f75fa67875c518c
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-05-04 20:19:58 +0100

    Update classlist.js

--- classlist.js
+++ classlist.js
@@ -23,3 +23,19 @@ El objeto .classList aunque parece que devuelve un  no es un array, sino un  que
 Por último, observa que disponemos del método .classList.item() 
   que nos devuelve un  con la clase específica en esa posición. 
     Si no existe una clase en esa posición, nos devolverá .
+    
+
+Añadir y eliminar clases CSS
+Los métodos .classList.add() y .classList.remove() permiten indicar una o múltiples clases CSS a añadir o eliminar. Observa el siguiente código donde se ilustra un ejemplo:
+
+const element = document.querySelector("#page");
+
+element.classList.add("uno", "dos");
+element.classList;  // ["info", "data", "dark", "uno", "dos"]
+
+element.classList.remove("uno", "dos");
+element.classList;  // ["info", "data", "dark"]
+
+Al utilizar los métodos .add() o .remove(), en el caso de que se añada una clase CSS 
+  que ya existía previamente, o que se elimine una clase CSS que no existía, 
+    simplemente no ocurrirá nada.
