edd6c273b7df81a656bed37992316028225cd88e
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-05-22 08:26:02 +0100

    Create errores_http.js

---
+++ errores_http.js
@@ -0,0 +1,13 @@
+Estos códigos responden a categorías de situación:
+𝟭𝟬𝟬–𝟭𝟵𝟵: 𝗥𝗲𝘀𝗽𝘂𝗲𝘀𝘁𝗮𝘀 𝗶𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝘃𝗮𝘀 
+𝟮𝟬𝟬–𝟮𝟵𝟵: 𝗥𝗲𝘀𝗽𝘂𝗲𝘀𝘁𝗮𝘀 𝘀𝗮𝘁𝗶𝘀𝗳𝗮𝗰𝘁𝗼𝗿𝗶𝗮𝘀,
+𝟯𝟬𝟬–𝟯𝟵𝟵: 𝗥𝗲𝗱𝗶𝗿𝗲𝗰𝗰𝗶𝗼𝗻𝗲𝘀, 
+𝟰𝟬𝟬–𝟰𝟵𝟵: 𝗘𝗿𝗿𝗼𝗿𝗲𝘀 𝗱𝗲 𝗹𝗼𝘀 𝗰𝗹𝗶𝗲𝗻𝘁𝗲𝘀,
+𝟱𝟬𝟬–𝟱𝟵𝟵: 𝗲𝗿𝗿𝗼𝗿𝗲𝘀 𝗱𝗲 𝗹𝗼𝘀 𝘀𝗲𝗿𝘃𝗶𝗱𝗼𝗿𝗲𝘀
+
+𝗠𝗶𝗿𝗮 𝗲𝗹 𝗣𝗗𝗙 𝗽𝗮𝗿𝗮 𝗰𝗼𝗻𝗼𝗰𝗲𝗿 𝗺á𝘀, 𝘆 𝘀𝘂𝗺𝗮𝗺𝗼𝘀:
+410 - Gone (ya no se encuentra el contenido)
+411 - Length Required (el campo content-lenght no fue especificado)
+412 - Precondition Failed (el servidor no cumple con las pre-condiciones que estableció el cliente)
+413 - Payload Too Large (la petición es más larga que los límites definidos por el servidor)
+414 - URL Too long (la URL pedida es demasiado larga para el servidor)
