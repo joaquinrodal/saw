550451a7b2e00b3edeae2bc9cc2c829cf415aa59
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-08-16 20:14:59 +0100

    Create tailwind_screen.js

---
+++ tailwind_screen.js
@@ -0,0 +1,21 @@
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+
+  class="text-[20px] bg-red-200 sm:text-[45px]" 
+
+  sm --> ancho hasta 640px 
+  sm:text-[45px] todo lo que es fuera del sm 640px sera 45px de tamaño 
+  y dentro del 640 el tamaño del texto sera de 20px , comprobado y funcionando.
