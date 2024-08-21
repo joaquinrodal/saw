0db3ef3440e0dd483b8fd1efeb792ec90caabb70
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-08-21 18:25:48 +0100

    Create 433.py

---
+++ 433.py
@@ -0,0 +1,47 @@
+
+
+EV1527: Tiene muchas más combinaciones posibles debido a los 20 bits de dirección, lo que proporciona mayor seguridad y menos posibilidad de interferencia.
+PT2262: Tiene menos combinaciones debido a sus 12 bits de dirección, lo que puede aumentar el riesgo de interferencia en áreas con muchos dispositivos.
+Tipo de Codificación:
+
+EV1527: Digital.
+PT2262: Analógica.
+Uso Moderno:
+
+EV1527: Es más común en aplicaciones modernas debido a su mejor seguridad y flexibilidad.
+PT2262: Todavía se usa, pero es más común en aplicaciones más antiguas o de bajo costo.
+
+
+  Codificadores EV1527 y PT2262
+EV1527 (o simplemente 1527):
+
+Bits de Dirección: El EV1527 usa 20 bits de dirección configurables y 4 bits de datos.
+Código: Genera un código de 24 bits. Cada control remoto basado en EV1527 tiene una dirección única que 
+  generalmente se establece en fábrica, aunque algunos dispositivos permiten configurar estos bits.
+Tipo: Es un chip de codificación digital.
+Aplicación: Muy utilizado en dispositivos modernos debido a su facilidad de configuración y compatibilidad c
+  on receptores de bajo costo.
+Ventaja: Proporciona una gran cantidad de combinaciones posibles para la dirección (2^20), lo que reduce 
+  el riesgo de interferencia con otros dispositivos similares en el área.
+PT2262 (o simplemente 2262):
+
+Bits de Dirección: El PT2262 usa hasta 12 bits de dirección y 4 bits de datos.
+Código: Genera un código de 12 bits de dirección y 4 bits de datos, lo que da un total de 16 bits.
+Tipo: Es un chip de codificación analógica.
+Aplicación: Es más antiguo y es común en controles remotos y sistemas de RF más simples o de bajo costo.
+Ventaja: La simplicidad de su diseño permite una fácil implementación, aunque tiene menos combinaciones 
+  posibles de direcciones (2^12), lo que significa que podría haber más interferencia en áreas con muchos dispositivos similares.
+
+Receptor RF de 433 Mhz, Módulo Decodificador de código de aprendizaje, salida inalámbrica de 4 canales de 433 MHz para controles remotos, codificación 1527 2262
+Área de aplicación del producto
+Interruptor de control remoto, módulo receptor, motocicletas, productos antirrobo para automóviles, productos de seguridad para el hogar, Puertas Eléctricas, puertas de persiana, ventanas, toma de control remoto, LED de control remoto, puertas Eléctricas de control remoto de audio remoto, control remoto de puerta de garaje, puertas retráctiles de control remoto, puerta de volumen remoto, puertas de sartén, Abridor de puerta por control remoto, sistema de control de dispositivo de cierre de puerta, cortinas de control remoto, host de alarma, alarma, control remoto de motocicleta de control remoto coches eléctricos, control remoto MP3.
+Aplicaciones
+1. Interruptor de alimentación inalámbrico, enchufe, interruptor de control remoto, módulo receptor,
+
+2. control de acceso, coches eléctricos, motocicletas, productos antirrobo para automóviles, abridores de puertas remotos, sistema de control de cierres
+
+3. Seguridad, sistemas de vigilancia, productos de seguridad para el hogar, Puertas Eléctricas, host de alarma, alarma
+
+4. Controles de habitaciones, puertas de persiana, ventanas, toma de control remoto, LED de control remoto, Puertas Eléctricas de control remoto de audio remoto, control remoto de puerta de garaje, puertas retráctiles de control remoto, puertas enrollables remotas, puerta corredera
+
+5. Productos inteligentes para el hogar, cortinas de control remoto, MP3 remoto, audio
