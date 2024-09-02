39fb6d416771c0d8f904207b87bf56daf6b34adc
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-09-02 18:20:44 +0100

    Create crm.py

---
+++ crm.py
@@ -0,0 +1,14 @@
+
+
+conda activate saturno
+
+173.249.21.118:8000
+
+python manage.py runserver 173.249.21.118:8000
+
+cd crm
+
+sudo docker ps -a
+sudo systemctl stop nginx
+sudo docker start contenedor
+
