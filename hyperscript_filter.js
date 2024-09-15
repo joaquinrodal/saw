a55f66afe24e1759f7d470de9571358c413259b4
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-09-15 13:28:30 +0100

    Create hyperscript_filter.js

---
+++ hyperscript_filter.js
@@ -0,0 +1,19 @@
+# hyperscript
+
+Quiero obtener un array filtrado por unas condiciones
+
+_.filter(al.items,o => {return o.name.includes(valor)})
+
+_,filter(array,condicion)
+
+condicion = funcion
+
+c => {return o.name.includes(valor}
+
+si quiero poner mas condiciones
+
+o.name.includes(valor) && o.name.includes(valor)  AND
+o.name.includes(valor) || o.name.includes(valor)  OR 
+
+
+
