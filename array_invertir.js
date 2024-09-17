bdb75daaf881dc959a9a8c49268195013d6e9bab
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-09-17 12:56:02 +0100

    Create array_invertir.js

---
+++ array_invertir.js
@@ -0,0 +1,12 @@
+#---------------
+  Invertir un array
+
+array_invertido = [...array.horarios].reverse() 
+
+array.horarios no queda afectado por invertir la lista
+
+array_invertido crea una lista invertida de array.horarios.
+
+#----------------
+
+  
