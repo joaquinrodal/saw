67d132444bc95d77dd723316fd1a00525bd5fb9f
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-12-23 19:45:52 +0000

    Create tmux.py

---
+++ tmux.py
@@ -0,0 +1,10 @@
+
+
+tmux list-sessions
+tmux attach -t nombre entorno
+tmux new -s nombre entorno 
+
+
+ctrl + b , --> cambiar nombre ventana
+ctrl + b c --> crear ventana 
+ctrl + b 1 2 3 4 --> ir a la ventana 1 2 3 4
