a35bb05cb0ff4bdc36987045868f9eebda2a4567
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-12-30 19:57:52 +0000

    Create cosas_de_array_js.js

---
+++ cosas_de_array_js.js
@@ -0,0 +1,13 @@
+
+
+tenemos un array 
+
+array = [0,1,2,3,4,5]
+
+y queremos cambiar el 0 por un 9
+
+lista = array.with(0,9) 
+
+crea una lista = nueva .
+
+  posicion 0 , y el valor que tiene que es un 0 lo cambia por un 9
