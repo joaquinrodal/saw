3124b90aa6b0a1b833caccc6e44c6991daba2acf
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2025-10-13 14:09:10 +0100

    Create cuando_cae_enwork.py

---
+++ cuando_cae_enwork.py
@@ -0,0 +1,25 @@
+
+
+Problemas con contado en Enwork
+
+
+
+
+Hay que crear tmux new -s rethink
+
+Y ejecutar rethink con
+rethinkdb --http-port 8086
+
+
+Luego crear Enwork
+
+Tmux new -s Enwork
+
+Conda activate Saturno 
+
+Cd Enwork
+
+Hypercorn - b 0.0.0.0:8200. servidor:app  
+
+direccion@enformate.net
+ENwork2024
